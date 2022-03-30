389fae0c4745d2f180facf162566bbf205174f00
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-30 08:47:19 -0300

    feat(src/shared/infra/http/routes/users.routes.ts): insert GET "/users". List all Users registered on App.

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,8 +1,11 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController"
+import { FindAllUsersController } from "@modules/users/useCases/findAllUsers/findAllUsersController"
 import { Router } from "express"
 
 export const usersRoutes = Router()
 
 const createUserController = new CreateUserController()
+const findAllUsersController = new FindAllUsersController()
 
 usersRoutes.post("/", createUserController.handle)
+usersRoutes.get("/", findAllUsersController.handle)
