ca398b31e4c4d82215ccfadea2319dd2b5f0aaa7
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-03-31 08:46:01 -0300

    test(src/modules/users/useCases/createUser/createUserController.spec.ts): insert at test new scenarios.

--- src/modules/users/useCases/createUser/createUserController.spec.ts
+++ src/modules/users/useCases/createUser/createUserController.spec.ts
@@ -16,4 +16,43 @@ describe("Create a User Controller", () => {
         expect(response.body).toHaveProperty("message")
         expect(response.body).toHaveProperty("user")
     })
+
+    it("Should not be able to craate a new User with username already exists", async () => {
+
+        const response = await request(app).post("/users").send({
+            name: "User Controller Error",
+            username: "user-controller",
+            email: "user.controller_error@email.com",
+            password: "controllerError"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty("message")
+    })
+
+    it("Should not be able to craate a new User with email already exists", async () => {
+
+        const response = await request(app).post("/users").send({
+            name: "User Controller Error",
+            username: "user-controller.error",
+            email: "user.controller@email.com",
+            password: "controllerError"
+        })
+
+        expect(response.status).toBe(400)
+        expect(response.body).toHaveProperty("message")
+    })
+
+    it("Should not be able to craate a new User with email missing parameters", async () => {
+
+        const response = await request(app).post("/users").send({
+            name: "User Controller Error",
+            email: "user.controller@email.com",
+            password: "controllerError"
+        })
+
+        expect(response.status).toBe(500)
+        expect(response.body).toHaveProperty("status")
+        expect(response.body).toHaveProperty("message")
+    })
 })
