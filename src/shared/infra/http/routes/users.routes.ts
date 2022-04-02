06f53f4038f5d0f6f80807caed523108d71c17c6
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-02 17:48:38 -0300

    feat(src/shared/infra/http/routes/users.routes.ts): insert GET "/users/{email}". Show an User registered on App by email address.

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,11 +1,14 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController"
 import { FindAllUsersController } from "@modules/users/useCases/findAllUsers/findAllUsersController"
+import { FindByEmailController } from "@modules/users/useCases/findByEmail/findByEmailController"
 import { Router } from "express"
 
 export const usersRoutes = Router()
 
 const createUserController = new CreateUserController()
 const findAllUsersController = new FindAllUsersController()
+const findByEmailController = new FindByEmailController()
 
 usersRoutes.post("/", createUserController.handle)
 usersRoutes.get("/", findAllUsersController.handle)
+usersRoutes.get("/:email", findByEmailController.handle)
