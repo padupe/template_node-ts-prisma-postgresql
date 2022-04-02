1ff8a573767460873c0ec7ccd20895d915c2ec40
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-02 18:00:16 -0300

    test(src/modules/users/useCases/findByEmail): create "findByEmailUseCase.spec.ts" at new directory "findByEmail".

---
+++ src/modules/users/useCases/findByEmail/findByEmailUseCase.spec.ts
@@ -0,0 +1,32 @@
+import { userDefault } from "@database/seed"
+import { UsersRepository } from "@modules/users/infra/prisma/repositories/UsersRepository"
+import { AppError } from "@shared/errors/appError"
+import { FindByEmailUseCase } from "./findByEmailUseCase"
+
+let findUserByEmailUseCase: FindByEmailUseCase
+let usersRepository: UsersRepository
+
+describe("Find an User by email address", () => {
+
+    beforeEach(() => {
+        usersRepository = new UsersRepository()
+        findUserByEmailUseCase = new FindByEmailUseCase(usersRepository)
+    })
+
+    it("Should be able to show an user by email address", async () => {
+
+        const user = await findUserByEmailUseCase.execute({email:userDefault.email})
+        expect(user).toHaveProperty("message")
+        expect(user).toHaveProperty("user")
+    })
+
+    it("Should not be able to show an unregistered User", async () => {
+
+        await expect(findUserByEmailUseCase.execute({email: "fail@fail.com"})).rejects.toEqual(new AppError("User not found!", 403))
+    })
+
+    it("Should not be able to show an User by invalid param", async () => {
+
+        await expect(findUserByEmailUseCase.execute({email: " "})).rejects.toEqual(new AppError("Invalid Parameters", 422))
+    })
+})
