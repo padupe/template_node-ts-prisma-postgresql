a89fa884b4b89eb7142b330430239cd169b9376f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-02 17:41:29 -0300

    fix(src/helpers/validateBodyUsers.ts): fix statusCode. Change "400" to "422".

--- src/helpers/validateBodyUsers.ts
+++ src/helpers/validateBodyUsers.ts
@@ -3,8 +3,24 @@ import { AppError } from "@shared/errors/appError";
 
 export function validateBodyCreateUser({ name, username, email, password }: ICreateUserDTO) {
 
-    if(name === "" || username === "" || email === "" || password === "") {
-        throw new AppError("Invalid Parameters", 400)
+    if(name === "" || name === undefined) {
+        throw new AppError("Invalid Name", 422)
+    }
+    
+    if(username === "" || username === undefined) {
+        throw new AppError("Invalid Username", 422)
+    }
+
+    if(email === "" || email === undefined) {
+        throw new AppError("Invalid Email", 422)
+    }
+    
+    if(password === "" || password === undefined) {
+        throw new AppError("Invalid Password", 422)
+    }
+    
+    if(name === "" && username === "" && email === "" && password === "") {
+        throw new AppError("Invalid Parameters", 422)
     }
 
     return true
