bb3350c4bf4f10747c1e5138feb921630aa21435
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-05 20:22:38 -0300

    feat(src/shared/infra/http/routes/users.routes.ts): insert GET "/users/id/{id}".

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController"
 import { FindAllUsersController } from "@modules/users/useCases/findAllUsers/findAllUsersController"
 import { FindUserByEmailController } from "@modules/users/useCases/findUserByEmail/findUserByEmailController"
+import { FindUserByIdController } from "@modules/users/useCases/findUserById/findUserByIdController"
 import { FindUserByUsernameController } from "@modules/users/useCases/findUserByUsername/findUserByUsernameController"
 import { Router } from "express"
 
@@ -9,9 +10,11 @@ export const usersRoutes = Router()
 const createUserController = new CreateUserController()
 const findAllUsersController = new FindAllUsersController()
 const findUserByEmailController = new FindUserByEmailController()
+const findUserByIdController = new FindUserByIdController()
 const findUserByUsernameController = new FindUserByUsernameController()
 
 usersRoutes.post("/", createUserController.handle)
 usersRoutes.get("/", findAllUsersController.handle)
 usersRoutes.get("/email/:email", findUserByEmailController.handle)
+usersRoutes.get("/id/:id", findUserByIdController.handle)
 usersRoutes.get("/username/:username", findUserByUsernameController.handle)
