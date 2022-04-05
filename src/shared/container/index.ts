776761750be6b84ef923c952b9c32d83d9422d15
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-04 22:10:23 -0300

    feat(src/shared/container/index.ts): inserts on file: "PostsRepository".

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,5 @@
+import { PostsRepository } from "@modules/posts/infra/prisma/repositories/PostsRepository"
+import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository"
 import { UsersRepository } from "@modules/users/infra/prisma/repositories/UsersRepository"
 import { IUsersRepository } from "@modules/users/repositories/IUsersRepository"
 import { container } from "tsyringe"
@@ -5,4 +7,9 @@ import { container } from "tsyringe"
 container.registerSingleton<IUsersRepository>(
     "UsersRepository",
     UsersRepository
+)
+
+container.registerSingleton<IPostsRepository>(
+    "PostsRepository",
+    PostsRepository
 )
