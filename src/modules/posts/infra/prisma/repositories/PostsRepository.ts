06b3e16a7273d8ec6c975575520dd36404b9e1bb
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-04 21:57:35 -0300

    feat(src/modules/posts): create "PostsRepository.ts" at "/infra/prisma/repositories". New directories at "src/modules/posts": "/infra/prisma/repositories".

---
+++ src/modules/posts/infra/prisma/repositories/PostsRepository.ts
@@ -0,0 +1,64 @@
+import { Posts, PrismaClient, Users } from "@prisma/client"
+import { prisma } from "@database/prismaClient"
+import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository"
+import { ICreatePostDTO } from "@modules/posts/dtos/ICreatePostDTO"
+
+export class PostsRepository implements IPostsRepository {
+    private repository: PrismaClient
+
+    constructor() {
+        this.repository = prisma
+    }
+
+    async create({ title, author_id }: ICreatePostDTO): Promise<Posts> {
+        const post = await this.repository.posts.create({
+            data: {
+                title,
+                author: author_id
+            }
+        })
+
+        return post
+    }
+
+    async find(post_id: string): Promise<Posts> {
+        const post = await this.repository.posts.findUnique({
+            where: {
+                id: post_id
+            },
+            select: {
+                id: true,
+                title: true,
+                user: {
+                    select: {
+                        username: true
+                    }
+                }
+            }
+        })
+
+        //@ts-ignore
+        return post
+    }
+
+    async update(post_id: string, title: string): Promise<Posts> {
+        const post = await this.repository.posts.update({
+            where: {
+                id: post_id
+            },
+            data: {
+                title
+            }
+        })
+
+        return post
+    }
+
+    async delete(post_id: string): Promise<void> {
+        await this.repository.posts.delete({
+            where: {
+                id: post_id
+            }
+        })
+    }  
+}
