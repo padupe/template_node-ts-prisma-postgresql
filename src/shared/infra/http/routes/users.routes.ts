4cea1d7742f67ea4fcf95e8f9b756aef5b3c1abb
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-04 21:53:08 -0300

    feat(src/shared/infra/http/routes/users.routes.ts): insert GET "/users/username/{username}". Show an User registered at App by Username.

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController"
 import { FindAllUsersController } from "@modules/users/useCases/findAllUsers/findAllUsersController"
 import { FindByEmailController } from "@modules/users/useCases/findByEmail/findByEmailController"
+import { FindByUsernameController } from "@modules/users/useCases/findByUsername/findByUsernameController"
 import { Router } from "express"
 
 export const usersRoutes = Router()
@@ -8,7 +9,9 @@ export const usersRoutes = Router()
 const createUserController = new CreateUserController()
 const findAllUsersController = new FindAllUsersController()
 const findByEmailController = new FindByEmailController()
+const findByUsernameController = new FindByUsernameController()
 
 usersRoutes.post("/", createUserController.handle)
 usersRoutes.get("/", findAllUsersController.handle)
-usersRoutes.get("/:email", findByEmailController.handle)
+usersRoutes.get("/email/:email", findByEmailController.handle)
+usersRoutes.get("/username/:username", findByUsernameController.handle)
