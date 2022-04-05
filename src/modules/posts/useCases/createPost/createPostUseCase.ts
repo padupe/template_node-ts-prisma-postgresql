4009aaabdacd69ec653094dcfba7a5ca44e17328
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-05 19:27:50 -0300

    feat(src/modules/posts/useCases/createPost): create "createPOstUseCase.ts".

---
+++ src/modules/posts/useCases/createPost/createPostUseCase.ts
@@ -0,0 +1,44 @@
+import "reflect-metadata"
+import { ICreatePostDTO } from "@modules/posts/dtos/ICreatePostDTO"
+import { IPostsRepository } from "@modules/posts/repositories/IPostsRepository"
+import { inject, injectable } from "tsyringe"
+import { IResponseCreatePostDTO } from "@modules/posts/dtos/IResponseCreatePostDTO"
+import { IUsersRepository } from "@modules/users/repositories/IUsersRepository"
+import { AppError } from "@shared/errors/appError"
+import { validateBodyPosts } from "validation/validateBodyPosts"
+
+@injectable()
+export class CreatePostUseCase {
+    constructor(
+        @inject("PostsRepository")
+        private postsRepository: IPostsRepository,
+
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ){}
+
+    async execute({ title, author_id }: ICreatePostDTO): Promise<IResponseCreatePostDTO>{
+
+        validateBodyPosts({ title, author_id })
+
+        const post = await this.postsRepository.create({
+            title,
+            author_id
+        })
+
+        const user = await this.usersRepository.findById(author_id)
+
+        if(!user) {
+            throw new AppError("User not found!", 403)
+        }
+
+        return {
+            message: "Post registered",
+            post: {
+                id: post.id,
+                title: post.id,
+                author: user.username
+            }
+        }
+    }
+}
