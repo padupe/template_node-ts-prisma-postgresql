b50a27dbe5dc103103ca9421a8b4f6e3f36c0721
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-05 20:21:57 -0300

    test(src/modules/users/useCases/findUserById): create "findUserByIdController.spec.ts".

---
+++ src/modules/users/useCases/findUserById/findUserByIdController.spec.ts
@@ -0,0 +1,36 @@
+import { userDefault } from "@database/seed"
+import { UsersRepository } from "@modules/users/infra/prisma/repositories/UsersRepository"
+import { app } from "@shared/infra/http/app"
+import request from "supertest"
+
+let usersRepository: UsersRepository
+
+describe("Find User by Id Controller", () => {
+
+    beforeEach(async () => {
+        usersRepository = new UsersRepository()
+    })
+
+    it("Should be able to show an User by id successfully", async () => {
+
+        let test = await usersRepository.findByUsername(userDefault.username)
+
+        console.log(test)
+
+        const response = await request(app).get(`/users/id/${test.id}`)
+
+        expect(response.status).toBe(200)
+        expect.objectContaining("id")
+        expect.objectContaining("username")
+    })
+    
+    it("Should not be able to show an User by invalid id", async () => {
+
+        let failedEmail = "fail"
+
+        const response = await request(app).get(`/users/id/${failedEmail}`)
+
+        expect(response.status).toBe(403)
+        expect.objectContaining("message")
+    })
+})
