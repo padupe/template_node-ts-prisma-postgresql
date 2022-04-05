34f0b04af789bd612ea950129b556cbabdd83347
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-05 19:26:03 -0300

    fix(src/shared/infra/http/routes/users.routes.ts): fix imports. After rename directories.

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -1,17 +1,17 @@
 import { CreateUserController } from "@modules/users/useCases/createUser/createUserController"
 import { FindAllUsersController } from "@modules/users/useCases/findAllUsers/findAllUsersController"
-import { FindByEmailController } from "@modules/users/useCases/findByEmail/findByEmailController"
-import { FindByUsernameController } from "@modules/users/useCases/findByUsername/findByUsernameController"
+import { FindUserByEmailController } from "@modules/users/useCases/findUserByEmail/findUserByEmailController"
+import { FindUserByUsernameController } from "@modules/users/useCases/findUserByUsername/findUserByUsernameController"
 import { Router } from "express"
 
 export const usersRoutes = Router()
 
 const createUserController = new CreateUserController()
 const findAllUsersController = new FindAllUsersController()
-const findByEmailController = new FindByEmailController()
-const findByUsernameController = new FindByUsernameController()
+const findUserByEmailController = new FindUserByEmailController()
+const findUserByUsernameController = new FindUserByUsernameController()
 
 usersRoutes.post("/", createUserController.handle)
 usersRoutes.get("/", findAllUsersController.handle)
-usersRoutes.get("/email/:email", findByEmailController.handle)
-usersRoutes.get("/username/:username", findByUsernameController.handle)
+usersRoutes.get("/email/:email", findUserByEmailController.handle)
+usersRoutes.get("/username/:username", findUserByUsernameController.handle)
