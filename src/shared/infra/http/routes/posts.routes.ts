68d2c98759e2d9b6ebb7f94cd46c1a1b7bcbdbf0
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-05 19:29:43 -0300

    feat(src/shared/infra/http/routes/posts.routes.ts): insert POST "/posts".

--- src/shared/infra/http/routes/posts.routes.ts
+++ src/shared/infra/http/routes/posts.routes.ts
@@ -1,3 +1,8 @@
+import { CreatePostController } from "@modules/posts/useCases/createPost/createPostController"
 import { Router } from "express"
 
 export const postsRoutes = Router()
+
+const createPostController = new CreatePostController()
+
+postsRoutes.post("/", createPostController.handle)
