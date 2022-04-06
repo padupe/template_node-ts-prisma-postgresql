8ecf173f9908f6d1bb2bc60a9bb536c2f70b61e8
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-06 09:05:22 -0300

    feat(src/shared/infra/http/routes/index.ts): update file. Import "authenticateRooutes" for "/authenticate".

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express"
+import { authenticateRoutes } from "./authenticate.routes"
 import { postsRoutes } from "./posts.routes"
 import { usersRoutes } from "./users.routes"
 
 export const router = Router()
 
 router.use("/users", usersRoutes)
+router.use("/authenticate", authenticateRoutes)
 router.use("/posts", postsRoutes)
