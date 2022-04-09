8a14d08b45ec35a6fa194bf640fee8a50ef6b9d9
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-09 09:39:12 -0300

    refactor(src/validation/validateBodyPosts.ts): change structure with base at JWT Authentication.

--- src/validation/validateBodyPosts.ts
+++ src/validation/validateBodyPosts.ts
@@ -1,18 +1,10 @@
 import { ICreatePostDTO } from "@modules/posts/dtos/ICreatePostDTO";
 import { AppError } from "@shared/errors/appError";
 
-export function validateBodyPosts ({ title, author_id }: ICreatePostDTO) {
+export function validateBodyPosts ({ title }: ICreatePostDTO) {
 
     if(title === "" || title === undefined) {
         throw new AppError("Invalid Title", 422)
-    }
-
-    if(author_id === "" || author_id === undefined) {
-        throw new AppError("Invalid Author ID", 422)
-    }
-
-    if(title === "" && author_id === "") {
-        throw new AppError("Invalid Parameters", 422)
     }
 
     return true
