d7ec036d07b2630e2b4bf6534019a4004ad034b1
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-11 07:45:49 -0300

    feat(src/shared/infra/http/routes/posts.routes.ts): insert GET "/posts/:id" and DELETE "/posts/:id".

--- src/shared/infra/http/routes/posts.routes.ts
+++ src/shared/infra/http/routes/posts.routes.ts
@@ -1,9 +1,15 @@
 import { CreatePostController } from "@modules/posts/useCases/createPost/createPostController"
 import { Router } from "express"
 import { ensureAuthenticateUser } from "@middlewares/ensureAuthenticateUser"
+import { DeletePostByIDController } from "@modules/posts/useCases/deletePostById/deletePostByIdController"
+import { FindPostByIdController } from "@modules/posts/useCases/findPostById/findPostByIdController"
 
 export const postsRoutes = Router()
 
 const createPostController = new CreatePostController()
+const findPostByIdController = new FindPostByIdController()
+const deletePostByIdController = new DeletePostByIDController()
 
 postsRoutes.post("/", ensureAuthenticateUser, createPostController.handle)
+postsRoutes.get("/:id", findPostByIdController.handle)
+postsRoutes.delete("/:id", ensureAuthenticateUser, deletePostByIdController.handle)
