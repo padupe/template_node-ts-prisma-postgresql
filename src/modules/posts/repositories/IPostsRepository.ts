3db99a8ccc35365a90445f81ced276a7d5ac60b3
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-11 07:37:04 -0300

    refactor(src/modules/posts/repositories/IPostRepository.ts): change "delete", for received "author_id".

--- src/modules/posts/repositories/IPostsRepository.ts
+++ src/modules/posts/repositories/IPostsRepository.ts
@@ -5,5 +5,5 @@ export interface IPostsRepository {
     create({ title, author_id }: ICreatePostDTO): Promise<Posts>
     find(post_id: string): Promise<Posts>
     update(post_id: string, title: string): Promise<Posts>
-    delete(post_id: string): Promise<void>
+    delete(post_id: string, author_id?: string): Promise<void>
 }
