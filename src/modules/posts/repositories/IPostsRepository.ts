3ed1516b71afa2cf57766e785ed19d218b292013
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-12 08:01:21 -0300

    refactor(src/modules/posts/repositories/IPostRepository.ts): change "update", for received "author_id".

--- src/modules/posts/repositories/IPostsRepository.ts
+++ src/modules/posts/repositories/IPostsRepository.ts
@@ -4,6 +4,6 @@ import { ICreatePostDTO } from "../dtos/ICreatePostDTO"
 export interface IPostsRepository {
     create({ title, author_id }: ICreatePostDTO): Promise<Posts>
     find(post_id: string): Promise<Posts>
-    update(post_id: string, title: string): Promise<Posts>
+    update(post_id: string, title: string, author_id?: string): Promise<Posts>
     delete(post_id: string, author_id?: string): Promise<void>
 }
