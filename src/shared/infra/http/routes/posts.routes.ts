8c455f5070e4877ccc3ed4629b54d14f1dd008f9
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-12 09:00:22 -0300

    feat(src/shared/infra/http/routes/posts.routes.ts): insert PUT "/posts/:id".

--- src/shared/infra/http/routes/posts.routes.ts
+++ src/shared/infra/http/routes/posts.routes.ts
@@ -3,13 +3,16 @@ import { Router } from "express"
 import { ensureAuthenticateUser } from "@middlewares/ensureAuthenticateUser"
 import { DeletePostByIDController } from "@modules/posts/useCases/deletePostById/deletePostByIdController"
 import { FindPostByIdController } from "@modules/posts/useCases/findPostById/findPostByIdController"
+import { UpdatePostByIdController } from "@modules/posts/useCases/updatePostById/updatePostByIdController"
 
 export const postsRoutes = Router()
 
 const createPostController = new CreatePostController()
 const findPostByIdController = new FindPostByIdController()
 const deletePostByIdController = new DeletePostByIDController()
+const updatePostByIdController = new UpdatePostByIdController()
 
 postsRoutes.post("/", ensureAuthenticateUser, createPostController.handle)
 postsRoutes.get("/:id", findPostByIdController.handle)
 postsRoutes.delete("/:id", ensureAuthenticateUser, deletePostByIdController.handle)
+postsRoutes.put("/:id", ensureAuthenticateUser, updatePostByIdController.handle)
