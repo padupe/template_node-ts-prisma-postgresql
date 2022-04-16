7a68b26f170bbb76d6edb814b25ceee1bfd82c39
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-16 17:41:43 -0300

    feat(src/auth/bcrypt): use logging with winston.

--- src/auth/bcrypt.ts
+++ src/auth/bcrypt.ts
@@ -1,5 +1,6 @@
 import { AppError } from "../shared/errors/appError"
 import { hash, compare } from "bcrypt"
+import { logging } from "utils/logging"
 
 export async function hashPassword(password: string) {
     return await hash(password, 10)
@@ -9,8 +10,10 @@ export async function verifyPassword(password: string, comparePassword: string) 
     const result = await compare(password, comparePassword)
     
     if (result === false) {
+        logging.error("User or Password Invalids")
         throw new AppError("User or Password Invalids", 401)
     }
 
+    logging.debug("verifyPassword Success.")
     return result
 }
