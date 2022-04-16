89afc47dfcb873f08b25dbc7323c5749c7bb187f
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-16 17:48:38 -0300

    feat(src/validation/validateAuthorId.ts): use logging with winston.

--- src/validation/validateAuthorId.ts
+++ src/validation/validateAuthorId.ts
@@ -1,5 +1,6 @@
 import { UsersRepository } from "@modules/users/infra/prisma/repositories/UsersRepository";
 import { AppError } from "@shared/errors/appError";
+import { logging } from "utils/logging";
 
 export async function validateAuthorId(author_id: string) {
 
@@ -9,8 +10,11 @@ export async function validateAuthorId(author_id: string) {
     const author = usersRepository.findById(author_id)
 
     if(!author) {
+        logging.error("validateAuthorId: User not found!")
         throw new AppError("User not found!", 400)
     }
+
+    logging.debug("validateAuthorId Success")
 
     return author
 }
