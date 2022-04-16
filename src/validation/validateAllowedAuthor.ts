85f296a124c7f707929f120511a3723a52e243fb
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-16 17:48:20 -0300

    feat(src/validation/validateAllowedAuthor): use logging with winston.

--- src/validation/validateAllowedAuthor.ts
+++ src/validation/validateAllowedAuthor.ts
@@ -1,10 +1,14 @@
 import { AppError } from "@shared/errors/appError";
+import { logging } from "utils/logging";
 
 export async function ValidateAllowedAuthor(post_author: string, author_id: string) {
 
     if(post_author != author_id) {
+        logging.error("validateAllowedAuthor: Invalid Token")
         throw new AppError("Invalid Token", 401)
     }
+
+    logging.debug("validateAllowedAuthor Success")
 
     return true
 }
