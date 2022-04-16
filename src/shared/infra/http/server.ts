0f93a54972334fea62b4681b9fc697c130ebf801
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-04-16 18:37:46 -0300

    feat(src/shared/infra/http/server.ts): use log.info.

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,6 +2,6 @@ import { logging } from "utils/logging"
 import { app } from "./app"
 
 app.listen(3000, () => {
-    logging.debug(`🚀 Server is runnnig!`)
+    logging.info(`🚀 Server is runnnig!`)
     console.log(`🚀 Server is runnnig!`)
 })
