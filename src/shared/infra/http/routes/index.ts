bcde75129dbce0538f053af1fa6eaebaaeab56a3
Paulo Eduardo Peixoto <pauloeduardo_peixoto@hotmail.com>
2022-06-08 00:31:39 -0300

    feat(src/shared/infra/http/routes/index.ts): update file with "index.ts".

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from "express"
+import { aliveRouter } from "./alive.route"
 import { authenticateRoutes } from "./authenticate.routes"
 import { postsRoutes } from "./posts.routes"
 import { usersRoutes } from "./users.routes"
@@ -8,3 +9,6 @@ export const router = Router()
 router.use("/users", usersRoutes)
 router.use("/authenticate", authenticateRoutes)
 router.use("/posts", postsRoutes)
+
+// Alive
+router.use("/alive", aliveRouter)
